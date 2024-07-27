2a57a812f7403e4ba55be4099c9095d1f2c7d43d
Ryan Chang <ryancyq@gmail.com>
2024-07-27 16:43:19 +0800

    initial commit

---
+++ lib/rspec/activesupport.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "activesupport/version"
+
+module Rspec
+  module Activesupport
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/rspec/activesupport_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Rspec::Activesupport do
+  it "has a version number" do
+    expect(Rspec::Activesupport::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end

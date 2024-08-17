b467c1cbb88dc932b343b902deb228cc6899079d
Ryan Chang <ryancyq@gmail.com>
2024-08-18 04:10:07 +0800

    add test cases for the RSpec matcher

--- spec/rspec/active_support_spec.rb
+++ spec/rspec/active_support_spec.rb
@@ -1,6 +1,14 @@
 # frozen_string_literal: true
 
+require "active_support/core_ext/string/inflections"
 require "rspec-activesupport"
 
 RSpec.describe RSpec::ActiveSupport do
+  it "defines deprecator on RSpec.configuration" do
+    expect(RSpec.configuration).to respond_to(:deprecator)
+  end
+
+  it "load matchers" do
+    expect("RSpec::ActiveSupport::Matchers::DeprecationMatcher".safe_constantize).to be_a(Module)
+  end
 end
